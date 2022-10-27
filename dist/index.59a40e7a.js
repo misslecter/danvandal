e49fbaea1969b312bc2767ae1846dab93d20e244
Anastasia <surikova.anastasia@gmail.com>
2022-10-27 21:08:03 +0200

    Scroll animations.

--- dist/index.59a40e7a.js
+++ dist/index.59a40e7a.js
@@ -532,8 +532,38 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"8lRBv":[function(require,module,exports) {
-const video = document.querySelector("video");
-if (video) video.play();
+const centaur = document.querySelector('[data-js-selector="centaur"]');
+const heading = document.querySelector('[data-js-selector="heading"]');
+const subheading = document.querySelector('[data-js-selector="subheading"]');
+const viewportHeight = window.innerHeight;
+// Centaur
+const centaurTargetXPosition = -(window.innerWidth / 2);
+const centaurTargetYPosition = viewportHeight * 0.2;
+const centaurTargetScale = 0.5;
+// Heading
+const headingTargetXPosition = window.innerWidth / 2;
+const headingTargetYPosition = -heading.getBoundingClientRect().height;
+// Subheading
+const subheadingTargetXPosition = window.innerWidth / 2;
+const subheadingTargetYPosition = viewportHeight * 0.3;
+// Apply default rotations
+const defaultRotation = `rotate(-15deg)`;
+window.addEventListener("scroll", (e)=>{
+    const percentageY = window.scrollY * 100 / viewportHeight;
+    // Centaur
+    const centaurCurrentXpx = centaurTargetXPosition * percentageY / 100;
+    const centaurCurrentYpx = centaurTargetYPosition * percentageY / 100;
+    const centaurCurrentScale = 1 - centaurTargetScale * percentageY / 100;
+    centaur.style.transform = `translateX(${centaurCurrentXpx}px) translateY(${centaurCurrentYpx}px) scale(${centaurCurrentScale})`;
+    // Heading
+    const headingCurrentXpx = headingTargetXPosition * percentageY / 100;
+    const headingCurrentYpx = headingTargetYPosition * percentageY / 100;
+    heading.style.transform = `${defaultRotation} translateX(${headingCurrentXpx}px) translateY(${headingCurrentYpx}px)`;
+    // Subheading
+    const subheadingCurrentXpx = subheadingTargetXPosition * percentageY / 100;
+    const subheadingCurrentYpx = subheadingTargetYPosition * percentageY / 100;
+    subheading.style.transform = `${defaultRotation} translateX(${subheadingCurrentXpx}px) translateY(${subheadingCurrentYpx}px)`;
+});
 
 },{}]},["7ZoMj","8lRBv"], "8lRBv", "parcelRequire245d")
 
