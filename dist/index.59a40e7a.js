5c0cbd703563508f8e62b817a36aa2a4c07bef4a
Anastasia <surikova.anastasia@gmail.com>
2023-05-05 20:37:23 +0200

    Animate ben&jerry.

--- dist/index.59a40e7a.js
+++ dist/index.59a40e7a.js
@@ -758,15 +758,35 @@ const iconsClassNames = (0, _portfolioJsonDefault.default).map((project)=>projec
 let previousY = window.scrollY;
 const handlePortfolioAnimations = (isScrollingDown)=>{
     // const isScrollingDown = window.scrollY > previousY;
-    console.log(isScrollingDown);
+    // console.log(isScrollingDown)
     for (const iconClassName of iconsClassNames){
         const element = document.querySelector(`.${iconClassName}`);
         if (!element) return;
-        const elementCenter = element.getBoundingClientRect().y + element.getBoundingClientRect().height / 2;
-        const shouldStartAnimation = elementCenter === window.innerHeight / 2;
+        const elementParent = element.parentElement;
+        const { x , y , height , width  } = element.getBoundingClientRect();
+        const elementCenter = y + height / 2; // center of element is decreasing when scrolling down
+        const screenCenter = window.innerHeight / 2;
+        const shouldHide = elementCenter <= screenCenter;
         switch(iconClassName){
             case "ben-jerry":
-                element.classList.toggle("animation--slide-right", shouldStartAnimation);
+                let interval;
+                if (shouldHide) {
+                    if (!element.classList.contains("animation--bounce") && !element.classList.contains("animation--slide-x")) {
+                        const behindRightScreenEdge = window.innerWidth - elementParent.getBoundingClientRect().right;
+                        element.style.setProperty("--translate-x", `${behindRightScreenEdge}px`);
+                        // Bounce
+                        const bounceDuration = 1000;
+                        element.classList.add("animation--bounce");
+                        // Slide right
+                        interval = setTimeout(()=>{
+                            element.classList.add("animation--slide-x");
+                        }, bounceDuration + 100);
+                    }
+                } else {
+                    if (interval) clearTimeout(interval);
+                    element.classList.remove("animation--bounce");
+                    element.classList.remove("animation--slide-x");
+                }
                 animateBenJerry(element);
         }
     }
--- src/js/modules/portfolio-animations.js
+++ src/js/modules/portfolio-animations.js
@@ -5,7 +5,7 @@ const iconsClassNames = portfolio.map(project => project.id);
 let previousY = window.scrollY;
 export const handlePortfolioAnimations = (isScrollingDown) => {
     // const isScrollingDown = window.scrollY > previousY;
-    console.log(isScrollingDown)
+    // console.log(isScrollingDown)
     for (const iconClassName of iconsClassNames) {
         const element = document.querySelector(`.${iconClassName}`);
 
@@ -13,12 +13,37 @@ export const handlePortfolioAnimations = (isScrollingDown) => {
             return;
         }
 
-        const elementCenter = element.getBoundingClientRect().y + element.getBoundingClientRect().height / 2;
-        const shouldStartAnimation = elementCenter === window.innerHeight / 2;
+        const elementParent = element.parentElement;
+        const {x, y, height, width} = element.getBoundingClientRect();
+        const elementCenter = y + height / 2; // center of element is decreasing when scrolling down
+        const screenCenter = window.innerHeight / 2;
+        const shouldHide = elementCenter <= screenCenter;
 
         switch (iconClassName) {
             case "ben-jerry":
-                element.classList.toggle('animation--slide-right', shouldStartAnimation)
+                let interval;
+                if (shouldHide) {
+                    if (!element.classList.contains('animation--bounce') && !element.classList.contains('animation--slide-x')) {
+                        const behindRightScreenEdge = window.innerWidth - elementParent.getBoundingClientRect().right;
+                        element.style.setProperty('--translate-x', `${behindRightScreenEdge}px`);
+
+                        // Bounce
+                        const bounceDuration = 1000;
+                        element.classList.add('animation--bounce')
+
+                        // Slide right
+                        interval = setTimeout(() => {
+                            element.classList.add('animation--slide-x')
+                        }, bounceDuration + 100);
+                    }
+                } else {
+                    if (interval) {
+                        clearTimeout(interval);
+                    }
+                    element.classList.remove('animation--bounce')
+                    element.classList.remove('animation--slide-x')
+                }
+
                 animateBenJerry(element);
         }
     }
